005a41d6df029976dffef69d73712048e8b1fe7c
Ryan Godoi da Silva <ryan.godoi.dasilva@gmail.com>
2024-12-01 01:07:50 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
+import { WebView } from 'react-native-webview';
+import NetInfo from '@react-native-community/netinfo';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+
+  const [internet, setInternet] = useState(false)
+  const image = require('./assets/backgrouds.jpg');
+  ;
+
+  useEffect(() => {
+    NetInfo.fetch().then((state) => {
+      setInternet(state.isConnected)
+    });
+  }, []);
+
+
+  const resetar = () => {
+    if (internet) {
+      console.log('tem internet')
+      setInternet(true)
+    } else {
+      alert('Você continua sem internet')
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      {internet ? (
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: 'https://jogodavelharyan.netlify.app' }}
+        />
+      ) : (
+        <ImageBackground style={styles.image} source={image}>
+          <View style={styles.mensagem}>
+            <Text style={styles.mensagemTexto}> Não tem internet!! </Text>
+            <TouchableOpacity onPress={() => resetar()} style={{ padding: 8, backgroundColor: 'green', borderRadius: 20 }}><Text style={styles.mensagemTexto2}>Reinciar App</Text></TouchableOpacity>
+          </View>
+        </ImageBackground>
+      )
+      }
     </View>
   );
 }
@@ -13,8 +51,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
+  },
+  mensagem: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  mensagemTexto: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  mensagemTexto2: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
   },
 });
